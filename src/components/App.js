a717bfb6f52777a6df28ca45a6fb792effcdf372
saiprakash15 <dsaip96@gmail.com>
2021-01-07 18:42:09 +0530

    initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+
+ const App  = () => 
+ {
+return (  
+     <div className ="ui container" style = {{marginTop: '5vh'}}>  
+     <SearchBar /> 
+     </div>);
+ };
+
+ export default App;
